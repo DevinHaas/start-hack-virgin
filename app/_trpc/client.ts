c93df35f44622249f52847e0472f865fa6819b11
Devin Halser <devin.hasler@students.ffhs.ch>
2025-03-20 15:39:25 +0100

    removed unused stuff

--- app/_trpc/client.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { createTRPCReact } from '@trpc/react-query'
-
-import type { PostRouter } from '@/app/server/routers/posts'
-
-export const trpc = createTRPCReact<PostRouter>({})
